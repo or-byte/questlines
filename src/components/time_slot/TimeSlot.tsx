354451d608248137608c35acbe89eb282262216f
czarbdizon-hub <czarbdizon@gmail.com>
2026-02-13 15:37:35 +0800

    Add StatusPill

--- src/components/time_slot/TimeSlot.tsx
+++ src/components/time_slot/TimeSlot.tsx
@@ -1,14 +1,16 @@
 import SlotContainer from "../slot/SlotContainer";
+import { JSX } from "solid-js"
 
 interface TimeSlotProps {
     time: string;
     price: string;
     isSelected: boolean;
+    onClick: JSX.EventHandlerUnion<HTMLDivElement, MouseEvent>;
 }
 
 export default function TimeSlot(props: TimeSlotProps) {
     return (
-        <SlotContainer isSelected={props.isSelected}>
+        <SlotContainer isSelected={props.isSelected} onClick={props.onClick}>
             <div class="flex flex-col items-start gap-[20px]">
                 <p class="subheader-1">{props.time}</p>
                 <p>{props.price}</p>
