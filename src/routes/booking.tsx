769463f86f1dfc3997f538e2a4c4beeaf11d2db0
czarbdizon-hub <czarbdizon@gmail.com>
2026-02-13 15:37:35 +0800

    Add StatusPill

--- src/routes/booking.tsx
+++ src/routes/booking.tsx
@@ -1,5 +1,5 @@
 import { Title } from "@solidjs/meta";
-import { createSignal } from "solid-js"
+import { createSignal, For } from "solid-js"
 import Button from "../components/button/Button";
 import Carousel from "~/components/carousel/Carousel";
 import CourtCard from "~/components/court_card/CourtCard";
@@ -7,6 +7,7 @@ import TimeSlot from "~/components/time_slot/TimeSlot";
 import DatePicker from "~/components/calendar/DatePicker";
 import BookingSummary from "~/components/summary/BookingSummary";
 import InfoPanel from "~/components/panel/InfoPanel";
+import StatusPill from "~/components/pill/StatusPill";
 
 export default function Booking() {
     const [date, setDate] = createSignal("2026-02-11");
@@ -15,20 +16,43 @@ export default function Booking() {
         'https://www.sportsimports.com/wp-content/uploads/How-to-Build-an-Outdoor-Pickleball-Court-.webp'
     ];
 
+    const courts = [
+        { id: 1, title: "Pickleball Court 1" },
+        { id: 2, title: "Pickleball Court 2" },
+        { id: 3, title: "Pickleball Court 3" },
+        { id: 4, title: "Pickleball Court 4" },
+        { id: 5, title: "Pickleball Court 5" },
+    ];
+
+    const timeSlots = [
+        { id: 1, time: "6:00 AM - 7:00 AM", price: "300.00" },
+        { id: 2, time: "6:00 AM - 7:00 AM", price: "300.00" },
+        { id: 3, time: "6:00 AM - 7:00 AM", price: "300.00" },
+        { id: 4, time: "6:00 AM - 7:00 AM", price: "300.00" },
+        { id: 5, time: "6:00 AM - 7:00 AM", price: "300.00" },
+    ];
+    const [selectedCourtId, setSelectedCourtId] = createSignal<number>(1);
+    const [selectedTimeId, setSelectedTimeId] = createSignal<number>(1);
+
     return (
         <main>
             <Title>Booking</Title>
             <div class="mx-30">
                 <h1 class="text-[var(--color-text-1)] text-justify">Cana Pickle Ball</h1>
                 <Carousel images={imageUrls} />
-                <div class="flex flex-col lg:flex-row gap-10 items-start mt-[20px]">
+                <div class="flex flex-col lg:flex-row gap-30 items-start mt-[20px]">
                     <div class="flex-1 min-w-0 space-y-10">
                         <div class="flex flex-col gap-[20px] w-full">
-                            <CourtCard title="Pickleball Court 1" isSelected={true} thumbnail="https://www.sportsimports.com/wp-content/uploads/How-to-Build-an-Outdoor-Pickleball-Court-.webp" />
-                            <CourtCard title="Pickleball Court 2" isSelected={false} thumbnail="https://www.sportsimports.com/wp-content/uploads/How-to-Build-an-Outdoor-Pickleball-Court-.webp" />
-                            <CourtCard title="Pickleball Court 3" isSelected={false} thumbnail="https://www.sportsimports.com/wp-content/uploads/How-to-Build-an-Outdoor-Pickleball-Court-.webp" />
-                            <CourtCard title="Pickleball Court 4" isSelected={false} thumbnail="https://www.sportsimports.com/wp-content/uploads/How-to-Build-an-Outdoor-Pickleball-Court-.webp" />
-                            <CourtCard title="Pickleball Court 5" isSelected={false} thumbnail="https://www.sportsimports.com/wp-content/uploads/How-to-Build-an-Outdoor-Pickleball-Court-.webp" />
+                            <For each={courts}>
+                                {(court) => (
+                                    <CourtCard
+                                        title={court.title}
+                                        thumbnail="https://www.sportsimports.com/wp-content/uploads/How-to-Build-an-Outdoor-Pickleball-Court-.webp"
+                                        isSelected={selectedCourtId() === court.id}
+                                        onClick={() => setSelectedCourtId(court.id)}
+                                    />
+                                )}
+                            </For>
                         </div>
                         <h2 class="text-[var(--color-text-1)] text-justify">Pickle Ball Court 1</h2>
                         <div class="flex flex-col items-baseline gap-[30px]">
@@ -40,10 +64,15 @@ export default function Booking() {
                         <div class="flex flex-col items-baseline gap-[30px]">
                             <h3 class="text-[var(--color-text-1)]">Select Time</h3>
                             <div class="grid grid-cols-2 gap-6 w-full">
-                                <TimeSlot isSelected={false} time="6:00 AM - 7:00 AM" price="300.00" />
-                                <TimeSlot isSelected={false} time="6:00 AM - 7:00 AM" price="300.00" />
-                                <TimeSlot isSelected={false} time="6:00 AM - 7:00 AM" price="300.00" />
-                                <TimeSlot isSelected={false} time="6:00 AM - 7:00 AM" price="300.00" />
+                                <For each={timeSlots}>
+                                    {(time) => (
+                                        <TimeSlot
+                                            time={time.time}
+                                            price={time.price}
+                                            isSelected={selectedTimeId() === time.id}
+                                            onClick={() => setSelectedTimeId(time.id)} />
+                                    )}
+                                </For>
                             </div>
                         </div>
                         <div>
@@ -62,14 +91,18 @@ export default function Booking() {
                     <aside class="w-full lg:w-[490px] shrink-0 space-y-10 lg:sticky lg:top-8">
                         <div class="flex justify-between w-full items-center">
                             <h3 class="text-[var(--color-text-1)]">Operating Hours</h3>
-                            <p class="body-3">Open</p>
+                            <StatusPill status="closed" />
+                        </div>
+                        <div class="flex justify-between pl-[30px]">
+                            <p class="body-2">Mon - Fri</p>
+                            <p class="body-2">6:00 AM - 12:00 AM</p>
                         </div>
                         <InfoPanel
                             email="sampleemail@gmail.com"
                             address="Address, address, address"
                             contact="+63 991 123 4561"
-                            facilities={[ "Facility1", "Facility2"]}
-                            rules={[ "rule1", "rule1"]}
+                            facilities={["Facility1", "Facility2"]}
+                            rules={["rule1", "rule1"]}
                         />
                     </aside>
                 </div>
