8c93b553579cef5d4a72222311df626a3baf48e3
czarbdizon-hub <czarbdizon@gmail.com>
2026-03-03 20:55:05 +0800

    Update PaymentResponse

--- src/components/response/PaymentResponse.tsx
+++ src/components/response/PaymentResponse.tsx
@@ -1,30 +1,34 @@
 import { MdRoundCheck_circle_outline } from 'solid-icons/md';
+import Button from "~/components/button/Button";
+import { useNavigate } from "@solidjs/router";
 
 type Response = {
     message: string,
     email: string,
     amount: string,
     currency: string,
-    paymentMethod: string,
-    cardType: string;
-    transactionId: string
+    paymentMethod: string
 }
 
 export default function PaymentResponse(props: Response) {
+    const navigate = useNavigate();
+    const toBooking = (path: string) => {
+        navigate(path);
+    };
+
     return (
         <main>
             <div class="flex flex-col gap-6 items-center w-full">
                 <MdRoundCheck_circle_outline size={100} color="var(--color-accent-1)" />
                 <div class="flex flex-col gap-4">
                     <h1>Payment Successful!</h1>
-                    <p class="body-1">{`Transaction ID:  ${props.transactionId}`}</p>
                 </div>
                 <div class="flex flex-col px-8 py-4 rounded-xl gap-6 w-full max-w-md md:max-w-xl">
                     <p class="subheader-1 font-bold">Payment Details</p>
                     <div class="flex flex-col gap-4">
                         <div class="flex justify-between">
                             <p class="body-1">Payment Method</p>
-                            <p class="body-1">{`${props.cardType?.toUpperCase()} ${props.paymentMethod}`}</p>
+                            <p class="body-1 ">{`${props.paymentMethod.toUpperCase()}`}</p>
                         </div>
                         <div class="flex justify-between">
                             <p class="body-1">Email</p>
@@ -34,7 +38,14 @@ export default function PaymentResponse(props: Response) {
                             <p class="body-1">Amout paid</p>
                             <p class="body-1">{`$${props.amount}`}</p>
                         </div>
+
                     </div>
+                    <Button
+                        onClick={[toBooking, "/booking/cana"]}
+                        class="btn w-full mt-4"
+                    >
+                        Go Back to Bookings
+                    </Button>
                 </div>
             </div>
         </main>
